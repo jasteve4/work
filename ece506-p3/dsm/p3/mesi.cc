f76b4b0d9da515886c797c1dd5972de1c3563051
Joshua Stevens <jasteve4@ncsu.edu>
2014-04-17 16:46:06 -0400

    new

---
+++ ece506-p3/dsm/p3/mesi.cc
@@ -0,0 +1,24 @@
+/*******************************************************
+			 dragon.cc
+		       Ed Gehringer
+		    efg@ncsu.edu, 2014
+********************************************************/
+		     
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include "main.h"
+#include "cache.h"
+#include "mesi.h"
+using namespace std;
+
+void MESI::PrRd(ulong addr, int processorNumber) 
+{
+}
+
+void MESI::PrWr(ulong addr, int processorNumber) 
+{
+}
+
+
+
---
+++ ece506-p3/dsm/p3/mesi.h
@@ -0,0 +1,23 @@
+/*******************************************************
+			 dragon.h
+		       Ed Gehringer
+		    efg@ncsu.edu, 2014
+********************************************************/
+
+#include "cache.h"
+
+class MESI : public Cache {
+ public:
+  MESI(int s, int a, int b, int p ): Cache(s, a, b, p) {};
+  ~MESI() { /* insert real code here */};
+  void PrRd(ulong addr, int processorNumber);
+  void PrWr(ulong addr, int processorNumber);
+  cacheLine *allocateLine(ulong addr) {return 0;}
+  void signalRd(ulong, int) {}
+  void signalRdX(ulong, int) {}
+  void signalUpgr(ulong, int) {}
+  void Int(ulong) {}
+  void Inv(ulong) {}
+  void notifyCtrl(ulong){}
+};
+
