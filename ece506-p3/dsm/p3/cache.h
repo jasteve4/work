061053661333feee5d73c2465ac4f9a037e3342c
jasteve4 <jasteve4@ncsu.edu>
2014-04-17 15:34:58 -0400

    create home for project

---
+++ ece506-p3/dsm/p3/cache.h
@@ -0,0 +1,139 @@
+/*******************************************************
+			  cache.h
+		 Ahmad Samih & Yan Solihin
+			   2009
+		     Ed Gehringer 2014
+	      {aasamih,solihin}@ece.ncsu.edu
+		       efg@ncsu.edu
+********************************************************/
+
+
+#ifndef CACHE_H
+#define CACHE_H
+
+#include <cmath>
+#include <iostream>
+
+typedef unsigned long ulong;
+typedef unsigned char uchar;
+typedef unsigned int uint;
+
+/****add new states, based on the protocol****/
+enum cache_state {
+	empty,
+	I, // invalid
+	E, // exclusive
+	M, // modified
+	Sc, // shared clean
+	Sm, // shared modified
+	V, // valid exclusive
+	S, // shared
+	D, // dirty
+};
+
+enum dir_state {
+  U, // unowned
+  EM, // exclusive-modified
+  S_, // shared
+};
+
+class dirEntry {
+  public:
+    ulong tag;
+    uchar state;
+    bool bit[4];
+
+    dirEntry() {
+	tag = 0; state = U;
+	for (int i = 0; i < 4; i++)
+	    bit[i] = false;
+    }
+
+};
+
+class Directory {
+  public:
+    ulong size;
+    dirEntry *entry;
+
+    Directory(ulong numEntries) {
+	size = numEntries;
+	entry = new dirEntry[size];
+    }
+
+    dirEntry* findUncached(ulong dir_tag);
+    dirEntry* findEntry(ulong);
+    int Access(ulong);
+};
+
+
+class cacheLine 
+{
+protected:
+   ulong tag;
+   cache_state State;   
+   ulong seq; 
+ 
+public:
+   cacheLine()			{ tag = 0; State = empty; }
+   ulong get_tag()       	{ return tag; }
+   cache_state get_state()	{ return State;}
+   ulong get_seq()       	{ return seq; }
+   void set_seq(ulong Seq)	{ seq = Seq;}
+   void set_state(cache_state state)	{ State = state;}
+   void set_tag(ulong a)		{ tag = a; }
+   void invalidate()		{ tag = 0; State = I;}
+   bool valid()  { return ((State != empty) && (State != I)); }
+     // Was: { return ((State) != empty); , but that's a bug.}
+   
+};
+
+class Cache {
+protected:
+  ulong size, lineSize, assoc;
+  ulong sets, log2Sets, log2Blk, tagMask, numLines, num_processors;
+  ulong reads, readMisses, writes, writeMisses, writeBacks;
+  ulong invalidations, interventions;
+  ulong memoryTransactions, cache2cache;
+  int cache_num;
+  Directory * directory;
+
+  cacheLine **cache;
+  ulong tagField(ulong addr)	{ return (addr >> (log2Blk) );}
+  ulong indexField(ulong addr)	{ return ((addr >> log2Blk) & tagMask);}
+  ulong calcAddr4Tag(ulong tag) { return (tag << (log2Blk));}
+
+public:
+    ulong currentCycle;  
+     
+    Cache(int, int, int, int);
+    virtual ~Cache() { delete cache;}
+
+    cacheLine * findLineToReplace(ulong addr);
+    virtual cacheLine * allocateLine(ulong) = 0;
+    virtual void notifyCtrl(ulong) = 0;
+    cacheLine * findLine(ulong addr);
+    cacheLine * getLRU(ulong);
+
+    ulong getRM(){return readMisses;} ulong getWM(){return writeMisses;} 
+    ulong getReads(){return reads;}   ulong getWrites(){return writes;}
+    ulong getWB(){return writeBacks;}
+    ulong getmemTrans() {return memoryTransactions;}
+    ulong getcache2cache() {return cache2cache;}
+	
+    void writeBack(ulong)   {writeBacks++;}
+    void printStats(int);
+    void updateLRU(cacheLine *);
+    void setDirectory (Directory *dir) {directory = dir;}
+
+    virtual void PrRd(ulong, int) = 0;
+    virtual void PrWr(ulong, int) = 0;
+
+    virtual void signalRd(ulong, int) = 0;
+    virtual void signalRdX(ulong, int) = 0;
+    virtual void signalUpgr(ulong, int) = 0;
+    virtual void Int(ulong) = 0;
+    virtual void Inv(ulong) = 0;
+};
+
+#endif
---
+++ ece506-p3/dsm/p3/main.cc
@@ -0,0 +1,128 @@
+/*******************************************************
+                        main.cc
+               Ahmad Samih & Yan Solihin,
+          {aasamih,solihin}@ece.ncsu.edu, 2009
+       revised by Ed Gehringer, efg@ncsu.edu, 2014      
+********************************************************/
+
+#include <stdlib.h>
+#include <assert.h>
+#include <fstream>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+#define NUM_PROCESSORS 4
+
+#include "cache.h"
+#include "mesi.h"
+
+const int max_processors = 16;
+int blk_size;
+Directory * directory;
+Cache* processor_cache[max_processors];
+
+// Return the number of sharers of the block at addr
+int sharers(ulong addr) {
+  int count = 0;
+  for (int i = 0; i < NUM_PROCESSORS; i++) {
+    if (processor_cache[i]->findLine(addr) != NULL) count++;
+  }
+  return count;
+}
+
+void sendInv(ulong addr, int proc_num) {
+    ulong dir_tag = addr >> (int) log2(blk_size);
+    dirEntry * dir_entry = directory->findEntry(dir_tag);
+    for (int i = 0; i<NUM_PROCESSORS; i++)
+	if (i != proc_num && dir_entry->bit[i]) {
+	    dir_entry->bit[i] = false;
+	    processor_cache[i]->Inv(addr);
+    }
+}
+
+void sendInt(ulong addr, int proc_num) {
+    ulong dir_tag = addr >> (int) log2(blk_size);
+    dirEntry * dir_entry = directory->findEntry(dir_tag);
+    for (int i = 0; i<NUM_PROCESSORS; i++)
+	if (i != proc_num && dir_entry->bit[i]) {
+	    processor_cache[i]->Int(addr);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    ifstream fin;
+    FILE * pFile;
+
+    if (argv[1] == NULL){
+	printf("input format: ");
+	printf("./dsm_cache <cache_size> <assoc> <block_size> <trace_file> \n");
+	exit(0);
+    }
+
+    int cache_size = atoi(argv[1]);
+    int cache_assoc= atoi(argv[2]);
+    blk_size   = atoi(argv[3]);
+    char *fname =  (char *)malloc(20);
+    fname = argv[4];
+    
+    //*******prints out simulator configuration here*******//
+    printf("===== 506 DSM MESIF FBV simulator configuration =====\n");
+    printf("L1_SIZE:\t\t%d\n", cache_size);
+    printf("L1_ASSOC:\t\t%d\n", cache_assoc);
+    printf("L1_BLOCKSIZE:\t\t%d\n", blk_size);
+    printf("NUMBER OF PROCESSORS:\t%d\n", NUM_PROCESSORS);
+    printf("TRACE FILE:\t\t%s\n", fname);
+ 
+    //*********************************************//
+    //******Create an array of caches here*********//
+    //*********************************************//	 
+    ulong dirSize = cache_size*NUM_PROCESSORS/blk_size;
+    directory = new Directory(dirSize);
+
+    for (int i = 0; i < NUM_PROCESSORS; i++) {
+      processor_cache[i] = new MESI(cache_size, cache_assoc, blk_size, i);
+      processor_cache[i]->setDirectory(directory);
+    }
+
+    ///******************************************************************//
+    //**  This opens the file to be read and reports an error if open  **//
+    //**  fails							       **//
+    ///******************************************************************//
+    pFile = fopen (fname,"r");
+    if (pFile == 0)
+    {	
+	printf("Trace file problem\n");
+	exit(0);
+    }
+
+    ///******************************************************************//
+    //**  Read trace file,line by line,				       **//
+    //**  each (processor#, operation, address)		   	       **//
+    //**  propagate each request down through memory hierarchy	       **//
+    //**  by calling cacheArray[processor#]->Access(...)	       **//
+    ///******************************************************************//
+    int proc_no;
+    char op;
+    int addr;
+    
+    while (fscanf(pFile, "%d %c %x", &proc_no, &op, &addr) != EOF) {
+      if (op == 'w')
+	processor_cache[proc_no]->PrWr((ulong) addr, proc_no);
+      else
+	processor_cache[proc_no]->PrRd((ulong) addr, proc_no);
+    }
+
+
+    fclose(pFile);
+
+    ///*****************************************//
+    //**  Print out statistics for all caches **//
+    ///*****************************************//
+    
+    for (int i=0; i<NUM_PROCESSORS; i++)
+    {
+	processor_cache[i]->printStats(i);
+    }
+}
