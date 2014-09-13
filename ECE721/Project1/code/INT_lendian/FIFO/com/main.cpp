ac1fa4efdad6916b285a9b2538fcb97127370937
Joshua Stevens <jasteve4@ncsu.edu>
2014-09-13 15:30:59 -0400

    update to renamer class

---
+++ ECE721/Project1/code/INT_lendian/FIFO/com/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "renamer.h"
+
+int main()
+{
+  //unsigned int i = -1;
+ /* cout << "========intizaing FreeFIFO==========" << endl;
+  FreeFIFO free_list (1024);
+  while(!free_list.full){
+  i++; 
+  free_list.Push(i);}
+  cout << "=========Begin==================" << endl;
+  while(!free_list.empty) cout << free_list.Pop() << endl;
+  cout << "=================================" << endl;
+  ActiveFIFO active_list (1024);
+  while(!active_list.full){
+  i++; 
+  active_list.Push(i);
+  }
+  cout << "=========Begin==================" << endl;
+  while(!active_list.empty) cout << active_list.Pop() << endl;
+  cout << "=================================" << endl;*/
+//  BranchCheckPoint branch_check_point (1024);
+ // cout << branch_check_point.checkpoint_GMB << endl;
+  
+  return 0;
+  
+}
